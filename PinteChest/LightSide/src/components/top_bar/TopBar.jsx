ca253926411d6e319c351bca4929949327ea12b5
Badrozgar <ozgarbadr@gmail.com>
2025-03-15 00:28:47 +0000

    creating top bar

--- PinteChest/LightSide/src/components/top_bar/TopBar.jsx
+++ PinteChest/LightSide/src/components/top_bar/TopBar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import "./TopBar.css";
+import UserButton from "../useButton/userButton";
 
 const TopBar = () => {
-  return <div>TopBar</div>;
+  return (
+    <div className="topBar">
+      <div className="search">
+        <img src="/general/search.svg" alt="" />
+        <input type="text" placeholder="Search" />
+      </div>
+      <UserButton />
+    </div>
+  );
 };
 
 export default TopBar;
