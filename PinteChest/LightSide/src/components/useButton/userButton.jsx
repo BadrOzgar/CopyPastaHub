7826849d59bb7fce99b76411d54274a9ac8fce94
Badrozgar <ozgarbadr@gmail.com>
2025-03-15 00:48:56 +0000

    added user options

--- PinteChest/LightSide/src/components/useButton/userButton.jsx
+++ PinteChest/LightSide/src/components/useButton/userButton.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import "./userButton.css";
+import { useState } from "react";
 const UserButton = () => {
-  const currentUser = false;
+  const currentUser = true;
+  const [open, setOpen] = useState(false);
   return currentUser ? (
     <div className="userButton">
       <img src="/general/noAvatar.png" alt="" />
-      <img src="/general/arrow.svg" alt="" />
+      <img
+        src="/general/arrow.svg"
+        onClick={() => setOpen((prev) => !prev)}
+        className="arrow"
+        alt=""
+      />
+      {open && (
+        <div className="userOptions">
+          <div className="userOption">Profile</div>
+          <div className="userOption">Settings</div>
+          <div className="userOption">Logout</div>
+        </div>
+      )}
     </div>
   ) : (
     <a href="/" className="loginLink">
